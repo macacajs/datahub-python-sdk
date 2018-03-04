7f63c80a8903d7aa90a93797217fab6cfa1e70a6
xudafeng <xudafeng@126.com>
2018-03-04 18:38:56 +0800

    first commit

---
+++ Makefile
@@ -0,0 +1,14 @@
+all: install
+install:
+	pip install sphinx
+	pip install twine
+	pip install pylint
+doc:
+	sphinx-build -b html docsrc docs
+build:
+	python setup.py sdist build
+	python setup.py bdist_wheel --universal
+upload: build
+	twine upload dist/*
+lint:
+	pylint datahub
---
+++ tests/macaca/sdk.py
@@ -0,0 +1,19 @@
+#
+# Testcase for DataHub
+#
+
+import pytest
+
+from macaca.sdk import DataHubSDK
+
+def test_init():
+    print(DataHubSDK)
+
+#datahub = DataHub(hostname = '127.0.0.1', port = '9200')
+
+#datahub.switchScene(hub='sample', pathname='test1', data={ 'currentScene': 'scene1' })
+
+#datahub.switchAllScenes(hub='sample', data={ 'currentScene': 'default' })
+
+
+
